86c225a79397dcd3cdfd59ee88f1cd3d9ea02e6e
wangshuhan <wangshuhan@kanzhun.com>
2026-04-29 14:39:33 +0800

    feat: 始终在侧边栏打开

--- src/extension.ts
+++ src/extension.ts
@@ -37,7 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
       panel = vscode.window.createWebviewPanel(
         "imagePreview",
         "Image Preview",
-        vscode.ViewColumn.Two,
+        vscode.ViewColumn.Beside,
         {
           enableScripts: true,
           retainContextWhenHidden: true,
